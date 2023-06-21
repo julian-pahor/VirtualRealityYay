6e24c99a40c382d06432c3f176c93e6d687502db
Muglamp <99298207+Muglamp@users.noreply.github.com>
2023-06-21 10:43:08 +0930

    creepy guy creeps on you

--- VRProject/Assets/Prefabs/CreepyGuy/CreepyGuy.cs
+++ VRProject/Assets/Prefabs/CreepyGuy/CreepyGuy.cs
@@ -10,12 +10,27 @@ public class CreepyGuy : MonoBehaviour
 
     [SerializeField] List<GameObject> waypointGOs = new List<GameObject>();
 
+    VisiblityDetector visiblityDetector;
+
+    public Transform player;
+
+    public float speed;
+    public float huntTime;
+
+    float timer;
+
+    enum State { Wander,Seek,Retreat}
+    State state;
+
     //public Transform goal;
     // Start is called before the first frame update
     void Start()
     {
-        navMeshAgent = GetComponent<NavMeshAgent>();
+        if (player == null) Debug.LogWarning("Creepy guy needs a target player to creep on");
 
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        visiblityDetector = GetComponent<VisiblityDetector>();
+        navMeshAgent.speed = speed;
         GetRandomWaypoint();       
     }
 
@@ -29,13 +44,55 @@ public class CreepyGuy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (navMeshAgent.remainingDistance > 0.1f)
+        switch(state)
         {
-            return;
+            case State.Seek:
+
+                if(visiblityDetector.IsVisible() || navMeshAgent.remainingDistance < 1)
+                {
+                    
+                    state = State.Retreat;
+                    GetRandomWaypoint();
+                    navMeshAgent.speed = speed * 4;
+                }
+
+
+                break;
+            case State.Retreat:
+
+                if (navMeshAgent.remainingDistance < 0.1f)
+                {
+                    navMeshAgent.speed = speed;
+                    state = State.Wander;
+                }
+
+                break;
+            case State.Wander:
+
+                timer += Time.deltaTime;
+                if(timer > huntTime)
+                {
+                    //don't try this if we have no player
+                    if (player == null)
+                        return;
+
+                    timer = 0;
+                    state = State.Seek;
+                    navMeshAgent.destination = player.transform.position;
+                }
+
+                //thank you gamedev dustin
+                if (navMeshAgent.remainingDistance > 0.1f)
+                {
+                    return;
+                }
+                else
+                {
+                    GetRandomWaypoint();
+                }
+                break;
         }
-        else
-        {
-            GetRandomWaypoint();
-        }
+
+      
     }
 }
